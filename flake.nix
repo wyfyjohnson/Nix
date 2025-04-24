36b8cbbc9abf6f39061bc5b7d441f33b1a8005c0
Wyatt <wyfy.johnson@gmail.com>
2025-04-24 11:49:25 -0700

    cleaning up flake

--- flake.nix
+++ flake.nix
@@ -12,12 +12,19 @@
   outputs = { self, nixpkgs, ... }@inputs: {
     # use "nixos", or your hostname as the name of the configuration
     # it's a better practice than "default" shown in the video
-    nixosConfigurations.fenrir = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./hosts/fenrir/configuration.nix
-        inputs.home-manager.nixosModules.default
-      ];
+    nixosConfigurations = {
+      fenrir = nixpkgs.lib.nixosSystem {
+        extraSpecialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/fenrir/configuration.nix
+        ];
+      };
+      jormungandr = nixpkgs.lib.nixosSystem {
+        extraSpecialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/jormungandr/configuration.nix
+        ];
+      }
     };
   };
 }
