8e925a7fa5357bb5905e4615e8389bf6fa06adfb
Wyatt <wyfy.johnson@gmail.com>
2025-04-26 14:03:26 -0700

    adding bash to config

--- hosts/fenrir/configuration.nix
+++ hosts/fenrir/configuration.nix
@@ -16,10 +16,6 @@
   networking.hostName = "fenrir"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
 
@@ -54,7 +50,6 @@
   # Fonts
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
-    liberation_ttf
   ];
 
 
@@ -75,22 +70,13 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.wyatt = {
     isNormalUser = true;
     description = "wyatt";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "input" "video" "libvirtd" "networkmanager" "wheel" ];
     packages = with pkgs; [
     #  thunderbird
     ];
@@ -107,6 +93,13 @@
   programs.starship.enable = true;
   programs.steam.enable = true;
   programs.kdeconnect.enable = true;
+  programs.bash = {
+    shellAliases = {
+      ls = "eza -1 --icons";
+      ff = "fastfetch";
+      jormungandr = "ssh wyatt@192.168.69.100";
+    };
+  };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -139,22 +132,11 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 services.avahi = {
   enable = true;
   nssmdns4 = true;
   openFirewall = true;
 };
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 
 }
